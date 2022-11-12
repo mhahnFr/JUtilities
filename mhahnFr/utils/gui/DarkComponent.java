ba3ea21319fdaf7ba925d2e65a60faa8fe1f428f
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-11-12 18:52:41 +0100

    Added `getComponent()` and `addToList()` to the DarkComponent

--- mhahnFr/utils/gui/DarkComponent.java
+++ mhahnFr/utils/gui/DarkComponent.java
@@ -1,20 +1,20 @@
 /*
- * JUtilities - Some utilities written for Java.
+ * SecretPathway - A MUD client.
  *
  * Copyright (C) 2022  mhahnFr
  *
- * This file is part of the JUtilities. This library is free software:
+ * This file is part of the SecretPathway. This program is free software:
  * you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package mhahnFr.utils.gui;
@@ -22,6 +22,7 @@ package mhahnFr.utils.gui;
 import javax.swing.JComponent;
 
 import java.awt.Color;
+import java.util.Collection;
 
 /**
  * This class is a wrapper around a given {@link JComponent} to add a universal
@@ -71,5 +72,12 @@ public class DarkComponent<T extends JComponent> {
                 component.setBackground(null);
             }
         }
+    }
+
+    public T getComponent() { return component; }
+
+    public DarkComponent<T> addToList(Collection<DarkComponent<? extends JComponent>> collection) {
+        collection.add(this);
+        return this;
     }
 }
