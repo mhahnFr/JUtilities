d253d121760d3e32684d1d4f4ec4f903cd910e4a
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-12-22 17:48:20 +0100

    Fixed typo in documentation

--- mhahnFr/utils/gui/SelectableLabel.java
+++ mhahnFr/utils/gui/SelectableLabel.java
@@ -1,20 +1,20 @@
 /*
- * JUtilities - Some utilities written for Java.
+ * SecretPathway - A MUD client.
  *
  * Copyright (C) 2018 - 2022  mhahnFr
  *
- * This file is part of the JUtilities. This library is free software:
+ * This file is part of the SecretPathway. This program is free software:
  * you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package mhahnFr.utils.gui;
@@ -59,7 +59,7 @@ public abstract class SelectableLabel extends JPanel {
 
     /**
      * Sets whether this label should be selected.
-     * By default such labels are not selected.
+     * By default, such labels are not selected.
      *
      * @param b whether this label should be selected
      */
