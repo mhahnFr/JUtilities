fde90c7fc47d8ea846601bf17246a2b2acce9e08
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-02 13:06:22 +0100

    Added a DocumentAdapter

---
+++ mhahnFr/utils/gui/DocumentAdapter.java
@@ -0,0 +1,40 @@
+/*
+ * JUtilities - Some utilities written for Java.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the JVMScript. This program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.utils.gui;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ * This class acts as an adapter for the {@link DocumentListener}.
+ *
+ * @author mhahnFr
+ * @since 02.01.23
+ */
+public class DocumentAdapter implements DocumentListener {
+    @Override
+    public void insertUpdate(DocumentEvent e) {}
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {}
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {}
+}
