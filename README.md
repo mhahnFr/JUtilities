b3d348294cc38b5d0dc9c734053074e9ac3d8478
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-09 18:09:49 +0100

    Corrected copyright notices

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ They are intended to be used as additional module in other projects.
 ### Final notes
 This library is licensed under the terms of the GPL 3.0.
 
-© Copyright 2017 - 2022 [mhahnFr](https://www.github.com/mhahnFr)
+© Copyright 2017 - 2023 [mhahnFr](https://www.github.com/mhahnFr)
--- mhahnFr/utils/gui/menu/MenuProvider.java
+++ mhahnFr/utils/gui/menu/MenuProvider.java
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2022  mhahnFr
  *
- * This file is part of the JVMScript. This program is free software:
+ * This file is part of the JUtilities. This library is free software:
  * you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
