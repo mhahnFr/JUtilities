bdb4da8ed453455358d2d93905ebb44baaa60b95
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-14 13:43:15 +0100

    Added documentation

--- mhahnFr/utils/json/JSONParser.java
+++ mhahnFr/utils/json/JSONParser.java
@@ -34,29 +34,57 @@ import java.util.*;
  * @since 13.01.23
  */
 public class JSONParser {
+    /** The stream to read the data from. */
     private final StringStream stream;
 
+    /**
+     * Constructs this parser using the given {@link StringStream}.
+     *
+     * @param stream the stream to read the data from
+     */
     public JSONParser(StringStream stream) {
         this.stream = stream;
     }
 
+    /**
+     * Skips the whitespaces that follow.
+     */
     private void skipWhitespaces() {
         while (stream.hasNext() && Character.isWhitespace(stream.peek())) {
             stream.skip();
         }
     }
 
+    /**
+     * Checks whether the next characters equal the given string.
+     * If that is the case, they are consumed, otherwise an exception
+     * is thrown.
+     *
+     * @param string the string expected to follow
+     */
     private void expect(final String string) {
         expectKeep(string);
         stream.skip(string.length());
     }
 
+    /**
+     * Checks whether the next characters equal the given string.
+     * If this is not the case, an exception is thrown.
+     *
+     * @param string the string expected to follow
+     */
     private void expectKeep(final String string) {
         if (!stream.peek(string)) {
             throw new RuntimeException("Expected \"" + string + "\"!");
         }
     }
 
+    /**
+     * Reads a string. It is expected to be in the format: {@code "< content >"}.
+     *
+     * @return the read string, without quotation
+     * @see #expect(String)
+     */
     private String readString() {
         expect("\"");
 
@@ -68,6 +96,13 @@ public class JSONParser {
         return buffer.toString();
     }
 
+    /**
+     * Reads a field. It is expected to be a string followed by a colon.
+     *
+     * @return the name of the read field
+     * @see #expect(String)
+     * @see #readString()
+     */
     private String readField() {
         final var toReturn = readString();
         skipWhitespaces();
@@ -75,6 +110,14 @@ public class JSONParser {
         return toReturn;
     }
 
+    /**
+     * Returns the field named by the given string in the given object.
+     *
+     * @param obj the object whose field should be returned
+     * @param name the name of the field
+     * @return the field of the given object named by the given string
+     * @throws NoSuchFieldException if the requested field does not exist in the given object
+     */
     private Field getField(final Object obj, final String name) throws NoSuchFieldException {
         final var c = obj.getClass();
 
@@ -87,6 +130,13 @@ public class JSONParser {
         }
     }
 
+    /**
+     * Peeks the given string in the underlying string. If the given
+     * string is following in the stream, it is consumed.
+     *
+     * @param string the string to be peeked
+     * @return whether stream was following in the stream
+     */
     private boolean peekConsume(final String string) {
         if (stream.peek(string)) {
             stream.skip(string.length());
@@ -95,9 +145,18 @@ public class JSONParser {
         return false;
     }
 
+    /**
+     * Tries to decode a raw value from the stream.
+     *
+     * @param c the class of the object to be read
+     * @return an object representing the read raw value
+     */
     private Object readRawValue(Class<?> c) {
         final var buffer = new StringBuilder();
-        while (stream.hasNext() && !(Character.isWhitespace(stream.peek()) || stream.peek(',') || stream.peek('}') || stream.peek(']'))) {
+        while (stream.hasNext() && !(Character.isWhitespace(stream.peek()) ||
+                                     stream.peek(',')                   ||
+                                     stream.peek('}')                   ||
+                                     stream.peek(']'))) {
             buffer.append(stream.next());
         }
         final var string = buffer.toString();
@@ -120,6 +179,14 @@ public class JSONParser {
         return Double.valueOf(string);
     }
 
+    /**
+     * Reads a string from the stream. Returns an enum representation if
+     * the given class is an enum.
+     *
+     * @param c the class of the object to be read
+     * @return a string or an enum representation depending on the given class
+     * @throws ReflectiveOperationException if the class is an enum and its {@code valueOf} method cannot be invoked
+     */
     private Object readStringEnum(Class<?> c) throws ReflectiveOperationException {
         final var buffer = readString();
         if (Enum.class.isAssignableFrom(c)) {
@@ -128,6 +195,14 @@ public class JSONParser {
         return buffer;
     }
 
+    /**
+     * Reads an array from the stream.
+     *
+     * @param c the class of the array to be read
+     * @param type the generic type that might be present
+     * @return an array consisting of the objects read from the stream
+     * @throws ReflectiveOperationException if an object cannot be filled with the values
+     */
     private Object readArray(Class<?> c, Type type) throws ReflectiveOperationException {
         final var underlying = c.componentType();
 
@@ -146,6 +221,14 @@ public class JSONParser {
         return toReturn;
     }
 
+    /**
+     * Reads a collection from the stream.
+     *
+     * @param c the class of the collection
+     * @param type the generic type of the collection
+     * @return a collection consisting of the objects read from the stream
+     * @throws ReflectiveOperationException if an object cannot be filled with the values
+     */
     @SuppressWarnings("unchecked")
     private Object readCollection(Class<?> c, Type type) throws ReflectiveOperationException {
         final Collection<Object> collection;
@@ -173,6 +256,14 @@ public class JSONParser {
         return collection;
     }
 
+    /**
+     * Reads a mapping from the stream.
+     *
+     * @param c the class of the map
+     * @param type the generic type of the map
+     * @return a map consisting of the objects read from the stream
+     * @throws ReflectiveOperationException if an object cannot be filled with the values
+     */
     @SuppressWarnings("unchecked")
     private Object readMap(Class<?> c, Type type) throws ReflectiveOperationException {
         final Map<Object, Object> map;
@@ -214,6 +305,17 @@ public class JSONParser {
         return map;
     }
 
+    /**
+     * Reads an array, collection or mapping from the stream.
+     *
+     * @param c the class of the collection
+     * @param type the generic type of the collection
+     * @return a collection consisting of the read objects
+     * @throws ReflectiveOperationException if an object could not be filled with the values
+     * @see #readArray(Class, Type)
+     * @see #readCollection(Class, Type)
+     * @see #readMap(Class, Type)
+     */
     private Object readCollectionKind(Class<?> c, Type type) throws ReflectiveOperationException {
         final Object toReturn;
         if (c.isArray()) {
@@ -223,7 +325,7 @@ public class JSONParser {
         } else if (Map.class.isAssignableFrom(c)) {
             toReturn = readMap(c, type);
         } else {
-            // else problem!
+            // Problem!
             throw new RuntimeException("Unknown collection type!");
         }
         skipWhitespaces();
@@ -231,12 +333,32 @@ public class JSONParser {
         return toReturn;
     }
 
+    /**
+     * Reads an object from the stream.
+     *
+     * @param c the class of the object
+     * @return the read object
+     * @throws ReflectiveOperationException if the object could not be filled with the read values
+     */
     private Object readObjectKind(final Class<?> c) throws ReflectiveOperationException {
         final var value = c.getConstructor().newInstance();
         readInto(value);
         return value;
     }
 
+    /**
+     * Reads an object from the stream. Depending on the following characters,
+     * either a raw value, a collection or a normal object is read and returned.
+     *
+     * @param c the class of the object that should be read
+     * @param type the generic type of the object
+     * @return the read object
+     * @throws ReflectiveOperationException if the object could not be filled with the read values
+     * @see #readObjectKind(Class)
+     * @see #readCollectionKind(Class, Type)
+     * @see #readStringEnum(Class)
+     * @see #readRawValue(Class)
+     */
     private Object readObject(final Class<?> c, final Type type) throws ReflectiveOperationException {
         skipWhitespaces();
         if (stream.peek('{')) {
@@ -249,6 +371,13 @@ public class JSONParser {
         return readRawValue(c);
     }
 
+    /**
+     * Reads a field from the stream and fills it for the given object.
+     *
+     * @param obj the object to fill the field in
+     * @throws ReflectiveOperationException if an object could not be filled with the read values
+     * @see #readObject(Class, Type)
+     */
     private void readField(Object obj) throws ReflectiveOperationException {
         skipWhitespaces();
 
@@ -257,6 +386,13 @@ public class JSONParser {
         field.set(obj, readObject(field.getType(), field.getGenericType()));
     }
 
+    /**
+     * Reads the fields for the given object.
+     *
+     * @param obj the object to be filled
+     * @throws ReflectiveOperationException if an object could not be filled with the read values
+     * @see #readField(Object)
+     */
     private void readFields(Object obj) throws ReflectiveOperationException {
         skipWhitespaces();
         if (stream.peek('}')) return;
@@ -266,6 +402,12 @@ public class JSONParser {
         } while (peekConsume(","));
     }
 
+    /**
+     * Reads the values of the given object from the stream.
+     *
+     * @param obj the object to be filled
+     * @throws ReflectiveOperationException if an object could not be filled with the read values
+     */
     public void readInto(Object obj) throws ReflectiveOperationException {
         skipWhitespaces();
         expect("{");
