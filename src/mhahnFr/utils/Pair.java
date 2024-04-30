fd380bbd47d73ae645abaf3bf8fbd7e085dfd15a
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-04-30 16:43:04 +0200

    Documented the generic type parameters

--- src/mhahnFr/utils/Pair.java
+++ src/mhahnFr/utils/Pair.java
@@ -1,20 +1,22 @@
 /*
  * JUtilities - Some utilities written for Java.
  *
- * Copyright (C) 2017 - 2022  mhahnFr
+ * Copyright (C) 2017 - 2024  mhahnFr
  *
- * This file is part of the JUtilities. This library is free software:
- * you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * This file is part of the JUtilities.
  *
- * This library is distributed in the hope that it will be useful,
+ * JUtilities is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JUtilities is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * JUtilities, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package mhahnFr.utils;
@@ -22,6 +24,8 @@ package mhahnFr.utils;
 /**
  * This class combines to given objects.
  *
+ * @param <T> the first type
+ * @param <O> the second type
  * @author mhahnFr
  * @since 20.12.2017
  */
